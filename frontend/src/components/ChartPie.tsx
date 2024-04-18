597047d459b5ffb4336527624add3f59ec2b81ab
Sahar Levy <xvruii@gmail.com>
2024-04-18 21:30:05 +0300

    still working on those bugs

--- frontend/src/components/ChartPie.tsx
+++ frontend/src/components/ChartPie.tsx
@@ -1,37 +1,42 @@
 import { Chart } from "react-google-charts";
-import styles from "../styles/PriorityDataCardStyle.module.css"
+import styles from "../styles/PriorityDataCardStyle.module.css";
 
 interface ChartPieInterface {
-    LowPriorityCount: number;
-    MediumPriorityCount: number;
-    HighPriorityCount: number;
+  LowPriorityCount: number;
+  MediumPriorityCount: number;
+  HighPriorityCount: number;
 }
 
 const options = {
-    title: "My Tasks Priorities",
-    is3D: true,
-  };
+  title: "My Tasks Priorities",
+  is3D: true,
+  colors: ["#fdf1db", "#99f4ff", "#ffa3a3"],
+  pieSliceTextStyle: {
+    color: "black", // Change the color of the percentage numbers to black
+  },
+};
 
-const ChartPie = (PriorityProps : ChartPieInterface) => {
+const ChartPie = (PriorityProps: ChartPieInterface) => {
+  const data = [
+    ["Task", "Count"],
+    ["Low Priority", PriorityProps.LowPriorityCount],
+    ["Medium Priority", PriorityProps.MediumPriorityCount],
+    ["High Priority", PriorityProps.HighPriorityCount],
+  ];
 
-    const data = [
-        ["Task", "Count"],
-        ["Low Priority", PriorityProps.LowPriorityCount],
-        ["Medium Priority", PriorityProps.MediumPriorityCount],
-        ["High Priority", PriorityProps.HighPriorityCount],
-    ];
+  return (
+    <>
+      <div style={{ width: "100%", height: "100%" }}>
+        <Chart
+          chartType="PieChart"
+          data={data}
+          options={options}
+          width={"100%"}
+          height={"100%"}
+        />
+      </div>
+    </>
+  );
+};
 
-    return(
-<Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"100%"}
-      height={"90%"}
-      className={styles.InnerGraph}
-/>
-
-    )
-}
-
-export default ChartPie
+export default ChartPie;
--- frontend/src/components/PriorityDataCard.tsx
+++ frontend/src/components/PriorityDataCard.tsx
@@ -4,14 +4,12 @@ import * as HelperFunctions from "../HelperFunctions/TaskSorts";
 import PriorityCardStyle from "../styles/PriorityDataCardStyle.module.css";
 import ChartPie from "./ChartPie";
 
-
 interface PriorityDataCardInterface {
-    tasks: ITask[];
-    className?: React.CSSProperties;
-  }
+  tasks: ITask[];
+  className?: React.CSSProperties;
+}
 
-
-const PriorityDataCard = ({tasks, className}:PriorityDataCardInterface) => {
+const PriorityDataCard = ({ tasks, className }: PriorityDataCardInterface) => {
   const [lowPriorityCount, setLowPriorityCount] = useState(0);
   const [mediumPriorityCount, setMediumPriorityCount] = useState(0);
   const [highPriorityCount, setHighPriorityCount] = useState(0);
@@ -23,38 +21,64 @@ const PriorityDataCard = ({tasks, className}:PriorityDataCardInterface) => {
     setHighPriorityCount(prioritiesCount.high);
   }, [tasks]);
 
-    return(
-      <>
-        <div className={`${PriorityCardStyle.PriorityContainer} ${PriorityCardStyle.PositionRelative}`}>
-            <h4 className={`${PriorityCardStyle.CenterTextTitle}`}>Priority Dashboard</h4>
-          <div className={`${PriorityCardStyle.PriorityDashboardContainer}`}>  
-            <div className={`${PriorityCardStyle.LeftSideDivs}`}>
-                <div className={`${PriorityCardStyle.PriorityTotal} ${PriorityCardStyle.PriorityCardSize} ${PriorityCardStyle.PositionRelative}`}>
-                   <div className={`${PriorityCardStyle.PriorityFonts}`}>{lowPriorityCount + mediumPriorityCount + highPriorityCount}</div>
-                   <p className={`${PriorityCardStyle.CenterText}`}>Total Tasks</p>
-                </div>
-                <div className={`${PriorityCardStyle.PriorityLow} ${PriorityCardStyle.PriorityCardSize}`}>
-                  <div className={`${PriorityCardStyle.PriorityFonts}`}>{lowPriorityCount}</div>
-                  <p className={`${PriorityCardStyle.CenterText}`}>Low Priority</p>
-                </div>
+  return (
+    <>
+      <div
+        className={`${PriorityCardStyle.PriorityContainer} ${PriorityCardStyle.PositionRelative}`}
+      >
+        <h4 className={`${PriorityCardStyle.CenterTextTitle}`}>
+          Priority Dashboard
+        </h4>
+        <div className={`${PriorityCardStyle.PriorityDashboardContainer}`}>
+          <div className={`${PriorityCardStyle.LeftSideDivs}`}>
+            <div
+              className={`${PriorityCardStyle.PriorityTotal} ${PriorityCardStyle.PriorityCardSize} ${PriorityCardStyle.PositionRelative}`}
+            >
+              <div className={`${PriorityCardStyle.PriorityFonts}`}>
+                {lowPriorityCount + mediumPriorityCount + highPriorityCount}
+              </div>
+              <p className={`${PriorityCardStyle.CenterText}`}>Total Tasks</p>
             </div>
-           <div className={`${PriorityCardStyle.RightSideDivs}`}>
-           <div className={`${PriorityCardStyle.PriorityTotal} ${PriorityCardStyle.PriorityCardSize} ${PriorityCardStyle.PositionRelative}`}> 
-              <div className={`${PriorityCardStyle.PriorityFonts}`}>{mediumPriorityCount}</div>
-              <p className={`${PriorityCardStyle.CenterText}`}>Medium Priority</p>
-            </div>
-            <div className={`${PriorityCardStyle.PriorityTotal} ${PriorityCardStyle.PriorityCardSize} ${PriorityCardStyle.PositionRelative}`}>
-              <div className={`${PriorityCardStyle.PriorityFonts}`}>{highPriorityCount}</div>
-              <p className={`${PriorityCardStyle.CenterText}`}>High Priority</p>
-            </div>
-            </div>
-            <div className={`${PriorityCardStyle.Graph}`}>
-              <ChartPie LowPriorityCount={lowPriorityCount} MediumPriorityCount={mediumPriorityCount} HighPriorityCount={highPriorityCount}/>
+            <div
+              className={`${PriorityCardStyle.PriorityLow} ${PriorityCardStyle.PriorityCardSize}`}
+            >
+              <div className={`${PriorityCardStyle.PriorityFonts}`}>
+                {lowPriorityCount}
+              </div>
+              <p className={`${PriorityCardStyle.CenterText}`}>Low Priority</p>
             </div>
           </div>
+          <div className={`${PriorityCardStyle.RightSideDivs}`}>
+            <div
+              className={`${PriorityCardStyle.PriorityTotal} ${PriorityCardStyle.PriorityCardSize} ${PriorityCardStyle.PositionRelative}`}
+            >
+              <div className={`${PriorityCardStyle.PriorityFonts}`}>
+                {mediumPriorityCount}
+              </div>
+              <p className={`${PriorityCardStyle.CenterText}`}>
+                Medium Priority
+              </p>
+            </div>
+            <div
+              className={`${PriorityCardStyle.PriorityTotal} ${PriorityCardStyle.PriorityCardSize} ${PriorityCardStyle.PositionRelative}`}
+            >
+              <div className={`${PriorityCardStyle.PriorityFonts}`}>
+                {highPriorityCount}
+              </div>
+              <p className={`${PriorityCardStyle.CenterText}`}>High Priority</p>
+            </div>
+          </div>
+          <div className={`${PriorityCardStyle.Graph}`}>
+            <ChartPie
+              LowPriorityCount={lowPriorityCount}
+              MediumPriorityCount={mediumPriorityCount}
+              HighPriorityCount={highPriorityCount}
+            />
+          </div>
         </div>
-      </>
-    );
-  };
-  
-  export default PriorityDataCard;
+      </div>
+    </>
+  );
+};
+
+export default PriorityDataCard;
